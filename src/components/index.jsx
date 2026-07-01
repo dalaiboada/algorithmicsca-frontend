d15bfbe1ae31078b45060c5e82b9e181116d2d5d
dalaiboada <dalaiboadajob@gmail.com>
2026-07-01 12:06:43 -0400

    refactor(components): Crear archivo barril para la importación de componentes

---
+++ src/components/index.jsx
@@ -0,0 +1,8 @@
+import { Typography } from './custom/Typography';
+import { Input } from './custom/Input';
+import { PasswordField } from './custom/PasswordField';
+import { Checkbox } from './custom/Checkbox';
+import { Button } from './ui/button';
+import { Toaster } from './ui/sonner';
+
+export { Typography, Input, PasswordField, Checkbox, Button, Toaster };
