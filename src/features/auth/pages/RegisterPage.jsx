0615ea6f23d5a3a9cc7c302e50cc64091c7e5d47
dalaiboada <dalaiboadajob@gmail.com>
2026-07-01 16:10:29 -0400

    feat(Auth): Reemplazar placeholder ui con el formulario completo para RegisterPage

--- src/features/auth/pages/RegisterPage.jsx
+++ src/features/auth/pages/RegisterPage.jsx
@@ -1,10 +1,59 @@
-import { Link } from 'react-router-dom';
+import {
+  Typography,
+  Input,
+  PasswordField,
+  Checkbox,
+  Link,
+  Button,
+} from '@/components';
 
+import { Rocket, Mail } from 'lucide-react';
+
+import Logo from '/algorithmics-logo_largo.webp';
 export const RegisterPage = () => {
   return (
-    <>
-      <h1>Registrarse</h1>
-      <Link to="/auth">Iniciar sesión</Link>
-    </>
+    <div className="p-6">
+      <div className="flex items-center mb-2">
+        <img src={Logo} alt="Algorithmics Logo" className="w-auto h-8" />
+      </div>
+
+      <div className="mb-6">
+        <Typography variant="h2" color="dark">
+          Crea tu cuenta
+        </Typography>
+        <Typography color="gray">Únete y empieza tu aventura.</Typography>
+      </div>
+
+      <form className="space-y-4">
+        <div className="grid grid-cols-2 gap-4 mx-auto">
+          <Input type="text" label="Nombre" placeholder="ej. Victoria" />
+          <Input type="text" label="Apellido" placeholder="ej. Pérez" />
+        </div>
+
+        <Input
+          type="email"
+          label="Correo electrónico"
+          placeholder="tu@email.com"
+          icon={<Mail className="size-4" />}
+        />
+
+        <PasswordField />
+
+        <PasswordField
+          label="Confirmar contraseña"
+          placeholder="mín. 8 caracteres"
+        />
+
+        <div className="flex justify-center">
+          <Button variant="secondary" size="lg" className="mt-2 w-3/4">
+            Crear cuenta <Rocket className="size-4" />
+          </Button>
+        </div>
+      </form>
+
+      <Typography variant="bodySmall" color="gray" className="text-center pt-3">
+        ¿Ya tienes una cuenta? <Link to="/auth">Inicia sesión</Link>
+      </Typography>
+    </div>
   );
 };
