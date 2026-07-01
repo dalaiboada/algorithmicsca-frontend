00e5b1fe64342d13a0c3c6ef494af18ab8256bd8
dalaiboada <dalaiboadajob@gmail.com>
2026-07-01 11:58:56 -0400

    feat(routes): Habilitar rutas para el inición de sesión, registro y restablecimiento de contraseña

--- src/routes/index.jsx
+++ src/routes/index.jsx
@@ -5,21 +5,26 @@ import { ProtectedRoute } from '@/routes/ProtectedRoute';
 import { LoginPage } from '@/features/auth/pages/LoginPage';
 import { AuthLayout } from '@/layouts/AuthLayout';
 import { CoursesPage } from '@/features/shared/CoursesPage';
+import { RegisterPage } from '@/features/auth/pages/RegisterPage';
+import { ResetPasswordPage } from '@/features/auth/pages/ResetPasswordPage';
 
 export const router = createBrowserRouter([
   {
     element: <MainLayout />,
     children: [
-      { index: true, element: <Navigate to="/login" replace /> },
+      { index: true, element: <Navigate to="/auth" replace /> },
       { path: 'courses', element: <CoursesPage /> },
     ],
   },
   {
+    path: '/auth',
     element: <AuthLayout />,
     children: [
-      { path: 'login', element: <LoginPage /> },
-      // { path: '2fa', element: <TwoFactorPage /> },
-      // { path: 'reset-password', element: <ResetPasswordPage /> },
+      { index: true, element: <LoginPage /> },
+      { path: 'register', element: <RegisterPage /> },
+      { path: 'reset-password', element: <ResetPasswordPage /> },
+      // { path: '2fa-setup', element: <TwoFactorSetupPage /> },
+      // { path: '2fa-verify', element: <TwoFactorVerifyPage /> },
     ],
   },
   {
